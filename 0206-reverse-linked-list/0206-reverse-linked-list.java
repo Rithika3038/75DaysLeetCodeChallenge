132c069fab8b70b10cda5c7e0344e9927abff76a
Rithika3038 <2320040007@klh.edu.in>
2026-04-08 10:51:45 +0530

    Time: 0 ms (100%), Space: 44 MB (93.92%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,14 +1,10 @@
-class Solution {
-    public ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode next = null;
-        ListNode curr = head;
-        while (curr != null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        return prev;
+class Solution{
+    public ListNode reverseList(ListNode head){
+        if(head == null || head.next == null)
+        return head;
+        ListNode res = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return res;
     }
 }
